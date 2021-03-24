c501f854572b652a8c3b2c5d3518d849b11b1063
IanDG39 <bigjopoy@gmail.com>
2021-03-24 15:23:05 +0800

    progress on navbar

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-Hello Kimberly!
+<?php
+
+  include("php/inc/database.php");
+
+  // Redirect Admins
+  if (isset($_SESSION["account_type"]) && $_SESSION["account_type"] === "admin") {
+    header("location: viewUsers.php");
+  }
+
+  // Clients can/should still see posts even if they are not logged in, so index.php will contain the feed.
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Sociality</title>
+
+     <!-- Import Material Design Lite CSS -->
+     <link rel="stylesheet" href="mdl/material.min.css">
+     <!-- Import Material Design Lite Javascript -->
+     <script src="mdl/material.min.js" charset="utf-8"></script>
+     <!-- Import Material Design Icons from Google -->
+     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+   </head>
+   <body>
+
+       <!-- Uses a header that contracts as the page scrolls down. -->
+       <!-- Pasted CSS/HTML from MDL Documentation -->
+      <style>
+        .demo-layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type  {
+         padding-right: 0;
+        }
+      </style>
+
+      <div class="demo-layout-waterfall mdl-layout mdl-js-layout">
+        <!-- Navbar is too long, and is repeated in all pages so it is moved to a dedicated file. -->
+        <?php include_once("php/inc/navbar.php"); ?>
+
+       <main class="mdl-layout__content">
+         <div class="page-content">
+           <!-- Content stuff go here. -->
+           <h1>Peepeepoopoo</h1>
+         </div>
+       </main>
+      </div>
+
+   </body>
+
+ </html>
---
+++ php/inc/navbar.php
@@ -0,0 +1,145 @@
+<?php
+
+  /*
+    Site Title
+  */
+  $nav_siteTitle = "Sociality";
+
+  /*
+    Links are not edited in the HTML but prepared here.
+  */
+  $nav_folderName = "/php/";
+  $nav_guestLinks = [
+    "Log In" => "login.php",
+    "Register" => "register.php",
+  ];
+
+  $nav_userLinks = [
+    "Profile" => "profile.php",
+    "Liked Posts" => "likedPosts.php",
+    "Log Out" => "logout.php"
+  ];
+
+  $nav_adminLinks = [
+    "User List" => "adm_viewUsers.php",
+    "Posts List" => "adm_viewPosts.php"
+  ];
+ ?>
+
+<header class="mdl-layout__header mdl-layout__header--waterfall">
+  <!-- Top row, always visible -->
+  <div class="mdl-layout__header-row">
+    <!-- Title -->
+    <span class="mdl-layout-title"><?php echo $nav_siteTitle; ?></span>
+    <div class="mdl-layout-spacer"></div>
+
+  </div>
+  <!-- Bottom row, not visible on scroll -->
+  <div class="mdl-layout__header-row">
+    <div class="mdl-layout-spacer"></div>
+    <!-- Navigation -->
+    <nav class="mdl-navigation">
+
+      <!--
+        Display different links depending on $_SESSION["account_type"]
+      -->
+
+      <!-- Logged Out Links -->
+      <?php if(!isset($_SESSION["account_type"])): ?>
+        <?php foreach ($nav_guestLinks as $key => $value):?>
+          <!--
+            These links will be accessed from different locations,
+            So to cover for cases where the file clicking is not in
+            the same folder, this condition check exists.
+          -->
+          <?php if (strpos($_SERVER["PHP_SELF"], "/php/") !== false): ?>
+            <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+          <?php else: ?>
+            <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+          <?php endif; ?>
+        <?php endforeach; ?>
+
+      <!-- User Accounts -->
+      <?php elseif ($_SESSION["account_type"] === "user"): ?>
+        <?php foreach ($nav_userLinks as $key => $value):?>
+          <!--
+            These links will be accessed from different locations,
+            So to cover for cases where the file clicking is not in
+            the same folder, this condition check exists.
+          -->
+          <?php if (strpos($_SERVER["PHP_SELF"], "/php/") !== false): ?>
+            <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+          <?php else: ?>
+            <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+          <?php endif; ?>
+        <?php endforeach; ?>
+
+      <!-- Admin Accounts -->
+      <?php elseif ($_SESSION["account_type"] === "admin"): ?>
+        <?php foreach ($nav_adminLinks as $key => $value):?>
+          <!--
+            These links will be accessed from different locations,
+            So to cover for cases where the file clicking is not in
+            the same folder, this condition check exists.
+          -->
+          <?php if (strpos($_SERVER["PHP_SELF"], "/php/") !== false): ?>
+            <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+          <?php else: ?>
+            <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </nav>
+  </div>
+</header>
+<div class="mdl-layout__drawer">
+  <span class="mdl-layout-title"><?php echo $nav_siteTitle; ?></span>
+  <nav class="mdl-navigation">
+
+    <!-- Logged Out Links -->
+    <?php if(!isset($_SESSION["account_type"])): ?>
+      <?php foreach ($nav_guestLinks as $key => $value):?>
+        <!--
+          These links will be accessed from different locations,
+          So to cover for cases where the file clicking is not in
+          the same folder, this condition check exists.
+        -->
+        <?php if (strpos($_SERVER["PHP_SELF"], "/php/") !== false): ?>
+          <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+        <?php else: ?>
+          <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+        <?php endif; ?>
+      <?php endforeach; ?>
+
+    <!-- User Accounts -->
+    <?php elseif ($_SESSION["account_type"] === "user"): ?>
+      <?php foreach ($nav_userLinks as $key => $value):?>
+        <!--
+          These links will be accessed from different locations,
+          So to cover for cases where the file clicking is not in
+          the same folder, this condition check exists.
+        -->
+        <?php if (strpos($_SERVER["PHP_SELF"], "/php/") !== false): ?>
+          <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+        <?php else: ?>
+          <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+        <?php endif; ?>
+      <?php endforeach; ?>
+
+    <!-- Admin Accounts -->
+    <?php elseif ($_SESSION["account_type"] === "admin"): ?>
+      <?php foreach ($nav_adminLinks as $key => $value):?>
+        <!--
+          These links will be accessed from different locations,
+          So to cover for cases where the file clicking is not in
+          the same folder, this condition check exists.
+        -->
+        <?php if (strpos($_SERVER["PHP_SELF"], "/php/") !== false): ?>
+          <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+        <?php else: ?>
+          <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    <?php endif; ?>
+  </nav>
+</div>
