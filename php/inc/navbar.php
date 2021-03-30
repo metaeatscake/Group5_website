4123e2f3a00aec0caf85c7b9ce3fa067492e74da
IanDG39 <bigjopoy@gmail.com>
2021-03-30 16:58:02 +0800

    implemented icons in the navbar

--- php/inc/navbar.php
+++ php/inc/navbar.php
@@ -8,26 +8,46 @@
   /*
     Links are not edited in the HTML but prepared here.
   */
+  /* Icon format:
+      <i class='material-icons'></i>
+      Where the ICON NAME goes inside the <i></i>
+      https://material.io/resources/icons/?style=sharp
+  */
+
   $nav_folderName = "php/";
   $nav_guestLinks = [
-    "Home" => "../",
     "Log In" => "login.php",
     "Register" => "register.php",
   ];
+  $nav_guestLinks_icons = [
+    "Log In" => "<i class='material-icons>login</i>'",
+    "Register" => "<i class='material-icons'>face</i>"
+  ];
 
   $nav_userLinks = [
-    "Home" => "../",
     "Create A Post" => "createPost.php",
     "Profile" => "profile.php",
     "Liked Posts" => "likedPosts.php",
     "Log Out" => "logout.php"
   ];
+  $nav_userLinks_icons = [
+    "Create A Post" => "<i class='material-icons'>add</i>",
+    "Profile" => "<i class='material-icons'>account_circle</i>",
+    "Liked Posts" => "<i class='material-icons'>thumb_up</i>",
+    "Log Out" => "<i class='material-icons'>logout</i>"
+  ];
 
   $nav_adminLinks = [
-    "Home" => "../",
     "Add Admin User" => "adm_addUsers.php",
     "User List" => "adm_viewUsers.php",
-    "Posts List" => "adm_viewPosts.php"
+    "Posts List" => "adm_viewPosts.php",
+    "Log Out" => "logout.php"
+  ];
+  $nav_adminLinks_icons = [
+    "Add Admin User" => "<i class='material-icons'>person_add_alt</i>",
+    "User List" => "<i class='material-icons'>ballot</i>",
+    "Posts List" => "<i class='material-icons'>view_list</i>",
+    "Log Out" => "<i class='material-icons'>logout</i>"
   ];
 
   /*  Reduce code redundancy in the html area
@@ -36,6 +56,9 @@
   $nav_chosenArray = (isset($_SESSION["account_type"])) ?
     ($_SESSION["account_type"] === "user") ? $nav_userLinks : $nav_adminLinks
     : $nav_guestLinks;
+  $nav_chosenArray_icons = (isset($_SESSION["account_type"])) ?
+    ($_SESSION["account_type"] === "user") ? $nav_userLinks_icons : $nav_adminLinks_icons
+    : $nav_guestLinks_icons;
 
   $nav_greetings = (isset($_SESSION["username"])) ? "Hello, {$_SESSION["username"]}!" : "Hello, Guest!";
 
@@ -62,15 +85,29 @@
       -->
 
       <?php foreach ($nav_chosenArray as $key => $value):?>
+
         <!--
-          These links will be accessed from different locations,
-          So to cover for cases where the file clicking is not in
-          the same folder, this condition check exists.
-        -->
+          Change navbar display
+            Possible $nav_display values:
+              icons_only -> icons only
+              text_only -> use the text options only
+              both -> use both. (default)
+          -->
+        <?php
+          $nav_display = "both";
+
+          switch($nav_display):
+          case 'icons_only': $nav_itemOutput = $nav_chosenArray_icons[$key]; break;
+          case 'text_only': $nav_itemOutput = $key; break;
+          default: $nav_itemOutput = $nav_chosenArray_icons[$key]." ".$key;
+          endswitch
+         ?>
+
+
         <?php if (strpos($_SERVER["PHP_SELF"], $nav_folderName) !== false): ?>
-          <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+          <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $nav_itemOutput; ?></a>
         <?php else: ?>
-          <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+          <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $nav_itemOutput; ?></a>
         <?php endif; ?>
       <?php endforeach; ?>
 
@@ -89,17 +126,31 @@
     <!--
       Display different links depending on $_SESSION["account_type"]
     -->
-      <span class="mdl-navigation__link"><?php echo $nav_greetings ?></span>
+
     <?php foreach ($nav_chosenArray as $key => $value):?>
+
       <!--
-        These links will be accessed from different locations,
-        So to cover for cases where the file clicking is not in
-        the same folder, this condition check exists.
-      -->
+        Change navbar display
+          Possible $nav_display values:
+            icons_only -> icons only
+            text_only -> use the text options only
+            both -> use both. (default)
+        -->
+      <?php
+        $nav_display = "both";
+
+        switch($nav_display):
+        case 'icons_only': $nav_itemOutput = $nav_chosenArray_icons[$key]; break;
+        case 'text_only': $nav_itemOutput = $key; break;
+        default: $nav_itemOutput = $nav_chosenArray_icons[$key]." ".$key;
+        endswitch
+       ?>
+
+
       <?php if (strpos($_SERVER["PHP_SELF"], $nav_folderName) !== false): ?>
-        <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $key; ?></a>
+        <a class="mdl-navigation__link" href="<?php echo $value; ?>"> <?php echo $nav_itemOutput; ?></a>
       <?php else: ?>
-        <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $key; ?></a>
+        <a class="mdl-navigation__link" href="<?php echo "$nav_folderName$value"; ?>"> <?php echo $nav_itemOutput; ?></a>
       <?php endif; ?>
     <?php endforeach; ?>
   </nav>
