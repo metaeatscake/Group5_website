fcc0498aeabf7c0c7e7b6cf7d5c81ad761b73af4
Kimberly Edge <guzmankimberlyedgede@yahoo.com>
2021-04-07 11:12:31 +0800

    sitll working on duplicate record

--- php/handleEditProfile.php
+++ php/handleEditProfile.php
@@ -7,104 +7,27 @@
     header("location: adm_viewUsers.php");
     exit();
   }
-  if (!isset($_SESSION["account_type"]))
+  //if (!isset($_SESSION["account_type"]))
  ?>
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title><?php echo $db_username; ?> | Edit Profile</title>
+<?php 
+  $id = $_SESSION["account_id"];
+  
+  $username = $_POST["username"];
+  $bio = $_POST["bio"];
+  $sex = $_POST["sex"];
+  $email = $_POST["email"];
 
-     <!-- Import Material Design Lite CSS -->
-     <link rel="stylesheet" href="../mdl/material.min.css">
-     <!-- Import Material Design Lite Javascript -->
-     <script src="../mdl/material.min.js" charset="utf-8"></script>
-     <!-- Import Material Design Icons from Google -->
-     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  $row = $sql->query("SELECT * FROM tbl_users WHERE email = '".$email."' AND user_id != '".$id."'")->fetch_assoc();     
 
-     <!-- Shortcut Icon -->
-     <link rel="shortcut icon" href="images/assets/socialityLogo_transparent.png">
-
-     <!-- Custom CSS File -->
-     <link rel="stylesheet" href="../css/socialityOverrides.css">
-   </head>
-
-   <body>
-
-       <!-- Uses a header that contracts as the page scrolls down. -->
-       <!-- Pasted CSS/HTML from MDL Documentation -->
-
-      <!-- Uses a transparent header that draws on top of the layout's background -->
-      <style>
-      @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
-        .demo-layout-transparent {
-          /* REPLACE THIS IMAGE WITH A BETTER BACKGROUND */
-          /*background: url('php/images/assets/test (1).jpg') center / cover;*/
-          background: #ad5389;  /* fallback for old browsers */
-          background: -webkit-linear-gradient(to right, #3c1053, #ad5389);  /* Chrome 10-25, Safari 5.1-6 */
-          background: linear-gradient(to right, #3c1053, #ad5389); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        }
-        .demo-layout-transparent .mdl-layout__header,
-        .demo-layout-transparent .mdl-layout__drawer-button {
-          /* This background is dark, so we set text to white. Use 87% black instead if
-             your background is light. */
-          color: #cca8e6;
-        }
-      </style>
-
-      <div class="demo-layout-transparent mdl-layout mdl-js-layout">
-        <!-- Navbar is too long, and is repeated in all pages so it is moved to a dedicated file. -->
-        <?php include_once("inc/navbar.php"); ?>
-
-       <main class="mdl-layout__content">
-
-         <div class="page-content" align="center">
-
-           <!-- Edit Profile Card. -->
-           <?php 
-              $id = $_SESSION["account_id"];
-
-              $username = $_POST["username"];
-              $bio = $_POST["bio"];
-              $sex = $_POST["sex"];
-              $email = $_POST["email"];
-
-              $row = "SELECT * FROM tbl_users WHERE email = '".$email."'";           
-
-              /*$numOfRows = $row->num_rows();*/
-              //to check if there's a duplicate
-              //i'll leave the classes empty
-              if($stmt = $sql->prepare($row)) {
-                  /* execute query */
-                  $stmt->execute();
-
-                  /* store result */
-                  $stmt->store_result();
-
-                  $stmt->num_rows;
-
-                  $stmt->close();
-                if($email != $db_email){
-                  echo "<h3 style='text-align: center;'><br>Duplicate Record! This user already exist. </h3>";
-                  echo "<br><br>";
-                  echo "<center><a href='profile.php' class='' style='text-align: center;'>Go back</a></center>"; exit;
-                }
-              }
-
-              $data = $sql->query("UPDATE tbl_users SET username = 'username', bio = 'bio', sex = 'sex', email = 'email' WHERE user_id = '$id'");
-              header("location: ../");
-              exit();
-              ?>
-          </div>
-
-       </main>
-
-       <?php include_once("inc/footer.php"); ?>
-
-      </div>
-
-   </body>
-
- </html>
+  //to check if there's a duplicate
+  //i'll leave the classes empty
+  if($row != NULL) {
+    echo "<h3 style='text-align: center;'><br>Duplicate Record! This user already exist. </h3>";
+    echo "<br><br>";
+    echo "<center><a href='profile.php' class='' style='text-align: center;'>Go back</a></center>"; exit;
+  } else {
+    $data = $sql->query("UPDATE tbl_users SET username = 'username', bio = '$bio', sex = '$sex', email = '$email' WHERE user_id = '$id'");
+    header("location: editProfile.php");
+  exit();
+  }
+  ?>
