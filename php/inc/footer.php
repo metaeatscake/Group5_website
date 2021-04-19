138519c0c6ddf2fdbcd6d1f0ff6e2c3ce2d0e210
IanDG39 <bigjopoy@gmail.com>
2021-04-19 14:34:32 +0800

    updated footer

--- php/inc/footer.php
+++ php/inc/footer.php
@@ -1,8 +1,17 @@
 <footer class="mdl-mini-footer">
   <div class="mdl-mini-footer__left-section">
-    <div class="mdl-logo"><?php echo $nav_siteTitle; ?></div>
+    <div class="mdl-logo">
+      <img src="php/images/assets/sample2white.png" alt="socialityLogo.png" style="height:30px;">
+      Sociality
+    </div>
     <ul class="mdl-mini-footer__link-list">
       <li><a href="https://github.com/metaeatscake/Group5_website">Github</a></li>
+
+    </ul>
+  </div>
+  <div class="mdl-mini-footer__right-section">
+    <ul class="mdl-mini-footer__link-list">
+      <li>Dependencies: </li>
       <li><a href="https://getmdl.io/index.html">[Material Design Lite]</a></li>
       <li><a href="https://material.io/resources/icons/?style=baseline">[Material Icons]</a></li>
     </ul>
