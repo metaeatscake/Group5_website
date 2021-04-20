333b86bb727f90fdb976195cde5479b82ef665ed
IanDG39 <bigjopoy@gmail.com>
2021-04-20 09:42:08 +0800

    added HashIDs package from Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '51f89a66fcf42c611ca3460f758dbcdae606c9ac',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51f89a66fcf42c611ca3460f758dbcdae606c9ac',
+    ),
+    'hashids/hashids' => 
+    array (
+      'pretty_version' => '4.1.0',
+      'version' => '4.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8cab111f78e0bd9c76953b082919fc9e251761be',
+    ),
+  ),
+);
