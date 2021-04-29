8e60c2f9ec2429605a779cbf65800656253d9369
Nirvana <arlingtonpunzalan@gmail.com>
2021-04-29 10:06:00 +0700

    margin

--- php/profile.php
+++ php/profile.php
@@ -92,8 +92,8 @@
 
                 <!--PROFILE MENU BUTTONS, BELOW THE AREA OF PROFILE PAGE MENU TOP-->
                 <div class="w3-bar" style="background-color: white;">
-                  <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'myPosts')" style="margin:10px; margin-right: 250px;">My Posts</button>
-                  <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'about')" style="margin:10px; margin-right: 190px;">About</button>
+                  <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'myPosts')" style="margin:10px;">My Posts</button>
+                  <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'about')" style="margin:10px;">About</button>
                   <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'customizeProfile')" style="margin:10px;">Customize Profile</button>
                 </div>
               </div>
