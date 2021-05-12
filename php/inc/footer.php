d178a7ab4f205bc688a296e23c0b7cc3579e23a0
IanDG39 <bigjopoy@gmail.com>
2021-05-12 16:25:34 +0800

    restored old footer code

--- php/inc/footer.php
+++ php/inc/footer.php
@@ -7,15 +7,21 @@
   $file_sclLogoSrc = ($file_inPHPFolder) ? $file_sclLogoLocation: 'php/'.$file_sclLogoLocation;
 
  ?>
-<script src="https://kit.fontawesome.com/7f2eccabe0.js" crossorigin="anonymous"></script>
-<div class="footer">
-  <div id="button"></div>
-  <div id="container">
-    <div class="logo">
-      <img src="<?php echo $file_sclLogoSrc; ?>" alt="socialityLogo.png" style="height:30px;">
-      <a href="index.php">Sociality</a>
-      <i class="fab fa-github"></i>
-      <a href="https://github.com/metaeatscake/Group5_website" id="git">Github</a>
-</div>
-</div>
-</div>
+ <script src="https://kit.fontawesome.com/7f2eccabe0.js" crossorigin="anonymous"></script>
+ <div class="footer">
+   <div id="button"></div>
+   <div id="container">
+     <div class="logo">
+       <img src="<?php echo $file_sclLogoSrc; ?>" alt="socialityLogo.png" style="height:30px;">
+       <a href="index.php">Sociality</a>
+       <i class="fab fa-github"></i>
+       <a href="https://github.com/metaeatscake/Group5_website" id="git">Github</a>
+
+       <ul class="list">
+           <li>Dependencies: </li>
+           <li><a href="https://getmdl.io/index.html">[Material Design Lite]</a></li>
+           <li><a href="https://material.io/resources/icons/?style=baseline">[Material Icons]</a></li>
+       </ul>
+     </div>
+   </div>
+ </div>
