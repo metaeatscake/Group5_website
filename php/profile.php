fca9a54da93e2bffb8d97ced3bb5cb9fae8edd6f
JMx64 <jmdlego123tv@gmail.com>
2021-05-13 09:02:58 +0800

    edited navbar

--- php/profile.php
+++ php/profile.php
@@ -31,6 +31,7 @@
     <link rel="stylesheet" href="../css/socialityOverrides.css">
     <link rel="stylesheet" type="text/css" href="../css/profileStyles.css">
     <link rel="stylesheet" href="../css/w3.css">
+    <link rel="stylesheet" href="../css/navbar.css">
   </head>
   <body>
     <?php include_once("inc/_js_mdl_formAlert.php") ?>
@@ -93,7 +94,15 @@
               <div class="w3-bar" style="background-color: white;">
                 <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'myPosts')" style="margin:10px;">My Posts</button>
                 <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'about')" style="margin:10px auto;">About</button>
+                <div class="dropdown">
                 <button class="w3-bar-item w3-button tablink" onclick="opentabs(event,'customizeProfile')" style="margin:10px;">Customize Profile</button>
+                    <i class="fa fa-caret-down"></i>
+                  </button>
+                <div class="dropdown-content">
+                  <a href="#">Edit Profile</a>
+                  <a href="#">Edit Profile Picture and Banner</a>
+                </div>
+                </div>
               </div>
             </div>
 
@@ -288,108 +297,7 @@
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                 <style>
-                body {margin:0;font-family:Arial}
-
-                .topnav {
-                overflow: hidden;
-                background-color: #333;
-                }
-
-                .topnav a {
-                 float: left;
-                 display: block;
-                 color: #f2f2f2;
-                 text-align: center;
-                   padding: 14px 16px;
-                 text-decoration: none;
-                 font-size: 17px;
-                }
-
-                .active {
-                background-color: #ad5389;
-                 color: white;
-                }
-
-                .topnav .icon {
-                 display: none;
-                }
-
-                .dropdown {
-                float: left;
-               overflow: hidden;
-                }
-
-                .dropdown .dropbtn {
-                font-size: 17px;    
-                border: none;
-                outline: none;
-                color: white;
-                padding: 14px 16px;
-                background-color: inherit;
-                font-family: inherit;
-                margin: 0;
-                }
-
-                .dropdown-content {
-                display: none;
-                position: absolute;
-                background-color: #f9f9f9;
-                min-width: 160px;
-                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                z-index: 1;
-                }
-
-                .dropdown-content a {
-                float: none;
-                color: black;
-                padding: 12px 16px;
-                text-decoration: none;
-                display: block;
-                text-align: left;
-                }
-
-                .topnav a:hover, .dropdown:hover .dropbtn {
-                 background-color: #555;
-                 color: white;
-                 }
-
-                .dropdown-content a:hover {
-                  background-color: #ddd;
-                 color: black;
-                  }
-
-                .dropdown:hover .dropdown-content {
-                 display: block;
-                    }
-
-                @media screen and (max-width: 600px) {
-                .topnav a:not(:first-child), .dropdown .dropbtn {
-                display: none;
-                }
-                .topnav a.icon {
-                float: right;
-                display: block;
-                }
-                }
-
-                @media screen and (max-width: 600px) {
-                .topnav.responsive {position: relative;}
-                .topnav.responsive .icon {
-                position: absolute;
-                right: 0;
-                top: 0;
-                }
-                .topnav.responsive a {
-                float: none;
-                display: block;
-                text-align: left;
-                }
-                .topnav.responsive .dropdown {float: none;}
-                .topnav.responsive .dropdown-content {position: relative;}
-                .topnav.responsive .dropdown .dropbtn {
-                display: block;
-                width: 100%;
-                text-align: left;}
+                
                </style>
                 </head>
                 <body>
@@ -426,6 +334,7 @@
     </div>
   </body>
 </html>
+
 <script>
   function opentabs(tab, tabname) {
     var i, x, tablinks;
