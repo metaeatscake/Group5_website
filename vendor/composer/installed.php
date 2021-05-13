ec166719fff303e438306c377f46520e1156f75a
IanDG39 <bigjopoy@gmail.com>
2021-05-13 12:03:40 +0800

    added mysql dumper thru composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '51f89a66fcf42c611ca3460f758dbcdae606c9ac',
+    'reference' => '5e3f45cfcc69321dddcd129370b866d7af4faf8f',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '51f89a66fcf42c611ca3460f758dbcdae606c9ac',
+      'reference' => '5e3f45cfcc69321dddcd129370b866d7af4faf8f',
     ),
     'hashids/hashids' => 
     array (
@@ -28,6 +28,15 @@
       array (
       ),
       'reference' => '8cab111f78e0bd9c76953b082919fc9e251761be',
+    ),
+    'ifsnop/mysqldump-php' => 
+    array (
+      'pretty_version' => 'v2.9',
+      'version' => '2.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc9c119fe5d70af9a685cad6a8ac612fd7589e25',
     ),
   ),
 );
