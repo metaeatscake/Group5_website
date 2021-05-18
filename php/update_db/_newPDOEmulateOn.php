1849159d425b7d712eade3cf8071ebd90e35b8f4
IanDG39 <bigjopoy@gmail.com>
2021-05-18 15:30:56 +0800

    updater now creates the DB if it doesn't exist.

--- php/update_db/_newPDOEmulateOn.php
+++ php/update_db/_newPDOEmulateOn.php
@@ -6,7 +6,7 @@ $pdo_pass = '';
 $pdo_host = 'localhost';
 $pdo_db = 'socialitydb';
 $pdo_charset = 'utf8mb4';
-$pdo_conn = "mysql:host=$pdo_host;dbname=$pdo_db;charset=$pdo_charset";
+$pdo_conn = "mysql:host=$pdo_host";
 $pdo_config = [
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -15,6 +15,7 @@ $pdo_config = [
 
 try {
   $pdo = new PDO($pdo_conn, $pdo_user, $pdo_pass, $pdo_config);
+  $pdo->exec("CREATE DATABASE IF NOT EXISTS `socialitydb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci");
 } catch (\PDOException $e) {
   throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
