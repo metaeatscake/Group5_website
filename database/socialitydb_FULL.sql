a3192d85ed4e74781ef09582f44fecc78821d8ee
IanDG39 <bigjopoy@gmail.com>
2021-05-18 15:00:51 +0800

    db updater minor edits

--- database/socialitydb_FULL.sql
+++ database/socialitydb_FULL.sql
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `socialitydb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `socialitydb`;
 
-DELIMITER $$
+`DELIMITER $$`
 --
 -- Procedures
 --
