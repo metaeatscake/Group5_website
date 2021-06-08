ef98c387bea5b7c453e1bf18c370fa08d9f7f51b
Nirvana <arlingtonpunzalan@gmail.com>
2021-06-08 15:53:50 +0700

    wrapper of register

--- css/customStyles.php
+++ css/customStyles.php
@@ -62,8 +62,8 @@
   -moz-border-radius:5px;
   -webkit-border-radius:5px;
   background-color:#fff;
-  background: -webkit-linear-gradient(#6148bf, #fff);
-  background: linear-gradient(#6148bf, #fff);
+  background: -webkit-linear-gradient(to bottom, #fff 70%, #fff6e9);
+  background: linear-gradient(to bottom, #fff 70%, #fff6e9);
   overflow:auto;
   align-items: center;
   float: left;
