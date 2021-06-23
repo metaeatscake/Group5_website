ba281ea244b7ab3944aae873a81c725bd5be4f5a
IanDG39 <bigjopoy@gmail.com>
2021-06-23 14:57:31 +0800

    rewritten php code for viewProfile

--- php/viewProfile.php
+++ php/viewProfile.php
@@ -2,17 +2,33 @@
   //Get database and session.
   include_once("inc/database.php");
 
-  //Prefetch User data.
-  $pdoq_getUserData = $pdo->prepare("SELECT * FROM tbl_users WHERE user_id = :user_id");
-  $pdoq_getUserData->execute(['user_id' => $_GET["id"]]);
-  $user_dataArray = $pdoq_getUserData->fetch(PDO::FETCH_ASSOC);
+  //Decode hashed user ID, g_id as in $_*G*ET["*id*"]
+  $g_id = $_GET["id"];
+  $g_id = $hashId->decode($g_id);
+  $g_id = $g_id[0];
+  //echo $g_id;
 
-  //var_dump($user_dataArray);
+  //User ID of the user currently logged in.
+  $s_id = $_SESSION["account_id"];
 
-  //Prefetch User Stats.
-  $pdoq_getUserStats = $pdo->prepare("SELECT * FROM view_user_stats WHERE user_id = :user_id");
-  $pdoq_getUserStats->execute(['user_id' => $_GET["id"]]);
-  $user_statsArray = $pdoq_getUserStats->fetch(PDO::FETCH_ASSOC);
+  //Security: verify if this user ID exists
+  $pdoq_checkIfIdExists = $pdo->prepare("SELECT count(user_id) as verifyExists
+      FROM view_user_stats
+      WHERE user_id = :user_id");
+  $pdoq_checkIfIdExists->execute(["user_id" => $g_id]);
+  $verifyID = $pdoq_checkIfIdExists->fetch(PDO::FETCH_COLUMN);
+  //echo $verifyID;
+
+  //Return to index page if the user ID doesn't exist.
+  if ($verifyID !== 1) {
+    header("location: ../");
+  }
+
+  //Prepare user data now that it is verified to exist.
+  $pdoq_getUserData = $pdo->prepare("SELECT * FROM view_user_stats WHERE user_id = :user_id");
+  $pdoq_getUserData->execute(["user_id" => $g_id]);
+  $profileData = $pdoq_getUserData->fetch(PDO::FETCH_ASSOC);
+  //print_r( $profileData);
 
  ?>
  <!DOCTYPE html>
@@ -20,7 +36,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $user_dataArray['username']; ?> | Profile</title>
+    <title><?php echo $profileData['username']; ?> | Profile</title>
 
     <!-- Import Material Design Lite CSS -->
     <link rel="stylesheet" href="../mdl/material.min.css">
@@ -39,6 +55,9 @@
     <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/scrollbar.css">
+
+    <!-- JS Likebutton -->
+    <script src="ajax/xmlhttp.js" charset="utf-8"></script>
   </head>
   <body>
     <?php include_once("inc/_js_mdl_formAlert.php") ?>
@@ -50,7 +69,6 @@
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
       .demo-layout-transparent {
-        /* REPLACE THIS IMAGE WITH A BETTER BACKGROUND */
         /*background: url('php/images/assets/test (1).jpg') center / cover;*/
         background: #ad5389;  /* fallback for old browsers */
         background: -webkit-linear-gradient(#21094e, #6148bf);  /* Chrome 10-25, Safari 5.1-6 */
@@ -72,27 +90,24 @@
      <main class="mdl-layout__content">
 
        <div class="page-content mdl-grid">
-        <!-- ADD THE PROFILE CARD HERE. -->
+
           <div style="width: 900px; margin:auto; background: -webkit-linear-gradient(to right, #3c1053, #ad5389); align-content: center;">
 
             <div style="background-color: white; text-align: center; color: #405d9b">
 
-              <!--TEMPORARY ONLY, ONCE I KNOW HOW TO WORK ON PUTTING UPLOAD IMAGE ICON BESIDE THE PROFILE PICTURE-->
-              <p><img src="images/users_cover/_default.png" style="width: 100%; height: 380px;" class="cover"></p>
+              <?php //This is the cover photo ?>
+              <p><img src="<?php echo $profileData['cover_photo']; ?>" style="width: 100%; height: 380px;" class="cover"></p>
 
-              <!---CHANGE THIS WITH THE TEMPORARY IMG THAT I PUT <img src="images/users_cover/bglight.jpg" style="width: 100%;"> -->
+              <?php //Profile picture ?>
+              <img src="<?php echo $profileData['profile_pic']; ?>" id="profile_pic">
 
-              <!--TEMPORARY ONLY, ONCE I KNOW HOW TO WORK ON PUTTING UPLOAD IMAGE ICON BESIDE THE PROFILE PICTURE-->
-
-              <img src="images/users/_default.jpg" id="profile_pic">
-
-              <!--CHANGE THIS WITH THE TEMPORARY IMG THAT I PUT <img id="profile_pic" src="images/users/$db_profile_pic"> --> <br>
+               <br>
 
               <!--BELOW AREA OF PROFILE PICTURE-->
               <div id="profile-page-menu-top">
                 <!--Name of the User-->
                 <div id="profile-menu-username">
-                  <?php echo $user_dataArray['username']; ?>
+                  <?php echo $profileData['username']; ?>
                 </div>
               </div>
               <br>
@@ -114,90 +129,36 @@
             <!--BELOW THE PROFILE CARD AREA-->
             <div style="display: flex;">
 
-               <!--SPONSORED-->
-              <div style="min-height: 440px; flex:1;">
-                <div id="sponsored-bar">
-                  <b>Sponsored</b> <br>
-
-                  <div id="sponsored">
-                    <img id="sponsored-img" src="images/assets/mfi-dts.jpg">
-                    <br>
-                    <a href="#">MFI Dual Training System Program</a>
-                  </div>
-
-                  <div id="sponsored">
-                    <img id="sponsored-img" src="images/assets/mfi-shs.jpg">
-                    <br><br>
-                    <a href="#">MFI Senior High School Program</a>
-                  </div>
-
-                  <div id="sponsored">
-                    <img id="sponsored-img" src="images/assets/mfi-womencourse.png">
-                    <br>
-                    <a href="#">MFI Women in STEM</a>
-                  </div>
-
-                </div>
-                <div class="sponsored-bar-footer">
-                  <a href="#">Privacy</a> ·
-                  <a href="#">Terms</a> ·
-                  <a href="#">Advertising</a> ·
-                  <a href="#">Ad Choices</a> ·
-                  <a href="#">Cookies</a> ·
-                  <a href="#">More</a> ·
-                  <a href="#">Sociality</a> &copy 2021
-                </div>
-
-              </div>
-
                <!--POSTS AREA/ABOUT AREA/CUSTOMIZE PROFILE-->
               <div id="post-area-menu" style="flex:2.5; padding: 20px; padding-right: 0px;">
                 <!--CONTENT OF MY POSTS -->
                 <div id="myPosts" class="tabmenu">
+
                   <?php
 
-                    // Handling Like data.
-                    if (isset($_SESSION["account_id"])):
-
-                      // PDO: Prepare the user's like data so the posts can be marked.
-                      $pdoq_getUserLikedPosts = $pdo->prepare("SELECT post_id FROM tbl_feed_likes WHERE user_id = :user_id");
-                      $pdoq_getUserLikedPosts->execute(['user_id' => $_SESSION["account_id"]]);
+                      // Check which posts by this user is liked by the account logged in.
+                      $pdoq_getUserLikedPosts = $pdo->prepare("SELECT post_id
+                        FROM tbl_feed_likes
+                        WHERE user_id = :user_id");
+                      $pdoq_getUserLikedPosts->execute(['user_id' => $s_id]);
                       $user_liked_post_id = $pdoq_getUserLikedPosts->fetchAll(PDO::FETCH_COLUMN);
 
-                    endif;
-
+                      // Fetch this user's posts
+                      // ORDER BY clause can be changed.
+                      $pdoq_getUserPosts = $pdo->prepare("SELECT *
+                        FROM view_posts_full
+                        WHERE user_id = :user_id
+                        ORDER BY post_time DESC");
+                      $pdoq_getUserPosts->execute(['user_id' => $g_id]);
+                      $postData = $pdoq_getUserPosts->fetchAll(PDO::FETCH_ASSOC);
                   ?>
 
-                  <?php
-
-                    //PDO Style, get all data from tbl_feed.
-                    //Also implement variable sort process.
-
-                    $feed_dateFormat = "%M %d %Y, %H:%i:%s";
-
-                    //Don't touch.
-
-                    $feed_queryString = "SELECT f.*, u.*,
-                        DATE_FORMAT(f.post_time, '$feed_dateFormat') AS date_time,
-                        COUNT(c.comment_id) AS count_comments,
-                        COUNT(fl.like_id) AS count_likes
-                      FROM tbl_feed f
-
-                      JOIN tbl_users u ON (f.user_id = u.user_id)
-                      LEFT OUTER JOIN tbl_feed_likes fl ON (f.post_id = fl.post_id)
-                      LEFT OUTER JOIN tbl_comments c ON (f.post_id = c.post_id)
-
-                      GROUP BY f.post_id
-                      ORDER BY f.post_time DESC";
-
-                    $post_dataArray = $pdo->query($feed_queryString)->fetchAll(PDO::FETCH_ASSOC);
-                    // echo "<pre style='color:white;'>"; var_dump($post_dataArray); echo "</pre>";
+                  <?php //Loop through all of the user's posts
+                    foreach ($postData as $row):
                   ?>
-
-                  <?php foreach ($post_dataArray as $row): ?>
 
                    <?php
-                      // Like Data setup.
+                      // Like Data setup. Check if this specific posts is in the array of post IDs that the user likes.
                       $isLiked = (isset($user_liked_post_id) && in_array($row["post_id"], $user_liked_post_id));
 
                       // NOTE: First string is the color/text when the post IS LIKED, the other is when it is NOT liked.
@@ -207,18 +168,18 @@
                       //"Encrypted" POST ID because style.
                       $post_fancyID = $hashId->encode($row['post_id']);
 
-                      //String for building the link to handleLikePost.php
-                      $post_likeButton_href = "handleLikePost.php?id=$post_fancyID&returnTo=viewProfile.php";
-
                       //Prepare link for ViewPost.
                       $post_viewPost_href = "viewPost.php?id=$post_fancyID";
 
-                      $profileIDHolder = $row["user_id"];
-                      $profileLink = ($row["user_id"] === $_SESSION["account_id"]) ? "profile.php" : "viewProfile.php?id=$profileIDHolder";
+                      //Prepare like and comment count for each post.
+                      $post_likeCount = (isset($row['count_likes'])) ? $row['count_likes'] : 0;
+                      $post_commentCount = (isset($row['count_comments'])) ? $row['count_comments'] : 0;
+
+                      //Likebutton
+                      $js_likePostLink = "ajax/xmlhttp_likePost.php?id=".$post_fancyID;
                     ?>
 
-                    <?php if ($row['user_id'] == $_GET["id"]): ?>
-
+                    <!-- USER POSTS -->
                       <div class="feed_post" id="<?php echo 'p_'.$post_fancyID; ?>">
 
                         <div class="more-horiz">
@@ -232,7 +193,7 @@
                         </div>
 
                         <div class="feed_post_author">
-                          <a href="<?php echo $profileLink?>">
+                          <a href="Javascript:void(0)">
                             <?php echo $row["username"]; ?>
                           </a>
                         </div>
@@ -254,7 +215,7 @@
 
 
                         <!-- Only display image div if there is image. -->
-                        <?php if (isset($row["post_img"])): ?>
+                        <?php if (isset($row["post_img"]) && file_exists($row["post_img"])): ?>
                           <div class="feed_image">
                               <img src="<?php echo $row['post_img']; ?>" alt="<?php echo $row['post_img']; ?>">
                           </div>
@@ -262,13 +223,15 @@
 
                         <div class="feed_actions">
 
-                          <a href="<?php echo $post_likeButton_href; ?>" style="color:<?php echo $post_likeButton_color; ?>">
-                            <i class="material-icons">thumb_up</i><?php echo $row["count_likes"]; ?>
+                          <a href="Javascript:void(0)" style="color:<?php echo $post_likeButton_color; ?>"
+                            onClick="xml_likePost('<?php echo $post_fancyID ?>', '<?php echo $js_likePostLink ?>')">
+                            <i class="material-icons">thumb_up</i>
+                            <span><?php echo $post_likeCount; ?></span>
                           </a>
 
                           <a href="<?php echo $post_viewPost_href; ?>">
                             <span class="material-icons" style="color: #262626;">mode_comment</span>
-                            <span style="color:black;"><?php echo $row["count_comments"]; ?></span>
+                            <span style="color:black;"><?php echo $post_commentCount; ?></span>
                           </a>
 
                           <a href="#">
@@ -277,51 +240,44 @@
 
                         </div><hr>
                       </div><br>
-                    <?php endif; ?>
+
                   <?php endforeach; ?>
                 </div>
 
                 <!--CONTENT OF ABOUT -->
+                <!-- Had to be completely rewritten, lmao -Ian -->
                 <div id="about" class="tabmenu" style="display:none;">
                   <h2><b>About</b></h2>
 
-                  <?php
-                    if($user_dataArray['bio'] != NULL){
-                      echo "
-                        <h4><b>Bio</b></h4>
-                        <h7>{$user_dataArray['bio']}</h7>
-                      ";
-                    }
-                  ?>
+                  <h4> <b>Bio</b> </h4>
+                  <h7> <?php echo $profileData["bio"]; ?></h7>
 
                   <h4><b>Joined</b></h4>
-                  <h7><?php echo $user_dataArray['register_time']; ?></h7>
+                  <h7><?php echo $profileData["register_time"]; ?></h7>
 
                   <?php
-                    if($user_statsArray['count_comments'] != NULL){
-                      echo "
-                        <h4><b>Number of Comments</b></h4>
-                        <h7>{$user_statsArray['count_comments']}</h7>
-                      ";
-                    } else{
-                      echo "<h4><b>Number of Comment</b></h4>
-                            <h7>0</h7>";
-                    }
-                  ?>
+                    //Table data can be null, so these just set them to 0 if null.
+                    $u_totalComments = (isset($profileData["count_comments"]))
+                      ? $profileData["count_comments"]
+                      : 0; //Can also be set to say "No comments"
 
-                  <?php
-                    if($user_statsArray['count_post_likes'] != NULL){
-                      echo "
-                        <h4><b>Number of Likes</b></h4>
-                        <h7>{$user_statsArray['count_post_likes']}</h7>
-                      ";
-                    } else{
-                      echo "
-                        <h4><b>Number of Like</b></h4>
-                        <h7>0</h7>
-                      ";
-                    }
-                  ?>
+                    $u_totalPostLikes = (isset($profileData["count_post_likes"]))
+                      ? $profileData["count_post_likes"]
+                      : 0;
+
+                    $u_totalPosts = (isset($profileData["count_posts"]))
+                      ? $profileData["count_posts"]
+                      : 0;
+                   ?>
+
+                  <h4> <b> Total Number of Comments </b> </h4>
+                  <h7> <?php echo $u_totalComments; ?></h7>
+
+                  <h4> <b> Total Post Count </b> </h4>
+                  <h7> <?php echo $u_totalPosts; ?></h7>
+
+                  <h4> <b> Total Post Likes </b> </h4>
+                  <h7> <?php echo $u_totalPostLikes; ?></h7>
                 </div>
               </div>
             </div>
