f50922d3fd75c3746bc382f55f771742d01c91ea
IanDG39 <bigjopoy@gmail.com>
2021-06-24 15:32:51 +0800

    add more functions to db

--- php/update_db/addFunctions.php
+++ php/update_db/addFunctions.php
@@ -4,12 +4,13 @@
 
   //Drop Functions if they exist.
   $pdo->exec("DROP FUNCTION IF EXISTS `verify_user_id_exists`");
+  $pdo->exec("DROP FUNCTION IF EXISTS `verify_post_id_exists`");
 
   //Add functions.
   /*
     FUNC: VERIFY IF USER EXISTS
       IN: user id to be tested
-      OUT: 0 (false) OR 1 (true) 
+      OUT: 0 (false) OR 1 (true)
   */
   $pdo->exec("
   CREATE DEFINER=`root`@`localhost` FUNCTION `verify_user_id_exists`(prm_id INTEGER
@@ -28,6 +29,19 @@
   	END IF;
 
   RETURN intbool;
+  END");
+
+  $pdo->exec("
+  CREATE DEFINER=`root`@`localhost` FUNCTION `verify_post_id_exists`(p_post_id INTEGER
+  ) RETURNS int(11)
+  BEGIN
+  	DECLARE boolint INTEGER DEFAULT 0;
+
+    SELECT COUNT(post_id) INTO boolint
+  	FROM tbl_feed
+  	WHERE post_id = p_post_id;
+
+  RETURN boolint;
   END");
 
  ?>
